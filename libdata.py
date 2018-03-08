221a235566c80830d58ba8d48895187126ff3730
Yitong Sun <syitong@umich.edu>
2018-03-08 16:10:32 -0500

    add in Mnist read method and confusion matrix plot method.

--- libdata.py
+++ libdata.py
@@ -26,3 +26,38 @@ def unit_circle_ideal(gap,label_prob,samplesize):
     X = np.array(X)
     Y = np.array(Y)
     return X,Y
+
+def read_MNIST_data(filepath,obs=1000):
+    """
+    This method is sufficiently general to read in any
+    data set of the same structure with MNIST
+    """
+    with open(filepath,'rb') as f:
+        data = f.read()
+        offset = 0
+        if data[2] < 10:
+            length = 1
+        elif data[2] < 12:
+            length = 2
+        elif data[2] < 14:
+            length = 4
+        elif data[2] < 15:
+            length = 8
+        dim = list()
+        offset = offset + 4
+        for idx in range(data[3]):
+            dim.append(int.from_bytes(data[3+idx*4+1:3+idx*4+5],'big'))
+            offset = offset + 4
+        l = length
+        for idx in range(1,len(dim)):
+            l = l * dim[idx]
+        if obs > 0 and obs < dim[0]:
+            dim[0] = obs
+        X = np.empty((dim[0],l))
+        for idx in range(dim[0]):
+            for jdx in range(l):
+                index = offset + idx * l + jdx
+                X[idx,jdx] = data[index]
+        if l == 1:
+            X = X[:,0]
+        return X
